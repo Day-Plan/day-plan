a7829f13196e581dd1b4898c43a11324ab2d07da
Visakh Vijayan <vjnvisakh@gmail.com>
2023-09-30 21:21:38 +0530

    [DP-14] project initialisation (#3)
    * added dependencies (#1)
    
    * database connected and env initialised (#2)

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('beginnign');
+import { connectDatabase } from "./src/modules/database/connection";
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const app = express();
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+    console.log('server started');
+});
